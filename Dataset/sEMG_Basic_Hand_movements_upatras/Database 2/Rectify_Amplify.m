27d5f779efc9360602ef8bff89f27a34daeefb17
krupaanand312jc <mkar312jc@gmail.com>
2021-08-14 01:42:29 +0530

    Rectified and Amplified the Signal data

---
+++ Dataset/sEMG_Basic_Hand_movements_upatras/Database 2/Rectify_Amplify.m
@@ -0,0 +1,17 @@
+for i=1:100
+    cyl_ch1(i,:) = (9/max(abs(cyl_ch1(i,:))))*(abs(cyl_ch1(i,:)));
+    hook_ch1(i,:) = (9/max(abs(hook_ch1(i,:))))*(abs(hook_ch1(i,:)));
+    lat_ch1(i,:) = (9/max(abs(lat_ch1(i,:))))*(abs(lat_ch1(i,:)));
+    palm_ch1(i,:) = (9/max(abs(palm_ch1(i,:))))*(abs(palm_ch1(i,:)));
+    spher_ch1(i,:) = (9/max(abs(spher_ch1(i,:))))*(abs(spher_ch1(i,:)));
+    tip_ch1(i,:) = (9/max(abs(tip_ch1(i,:))))*(abs(tip_ch1(i,:)));
+    
+    cyl_ch2(i,:) = (9/max(abs(cyl_ch2(i,:))))*(abs(cyl_ch2(i,:)));
+    hook_ch2(i,:) = (9/max(abs(hook_ch2(i,:))))*(abs(hook_ch2(i,:)));
+    lat_ch2(i,:) = (9/max(abs(lat_ch2(i,:))))*(abs(lat_ch2(i,:)));
+    palm_ch2(i,:) = (9/max(abs(palm_ch2(i,:))))*(abs(palm_ch2(i,:)));
+    spher_ch2(i,:) = (9/max(abs(spher_ch2(i,:))))*(abs(spher_ch2(i,:)));
+    tip_ch2(i,:) = (9/max(abs(tip_ch2(i,:))))*(abs(tip_ch2(i,:)));
+end
+    
+    
